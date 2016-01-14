fd4e1044032e2eda0ac3599810241bb5bbb73bc6
RUINAN <ruinan@RUINANs-MacBook-Pro.local>
2016-01-14 18:07:18 -0500

    Demo_Enegy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,12 +25,21 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
       
+//      get("/hello", (req, res) -> {
+//          RelativisticModel.select();
+//          Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+//          return "E=mc^2: 12 GeV = " + m.toString();
+//      });
+
       get("/hello", (req, res) -> {
           RelativisticModel.select();
-          Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-          return "E=mc^2: 12 GeV = " + m.toString();
+          
+          String energy = System.getenv().get("ENERGY");
+          
+          Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+          return "E=mc^2: " + energy + " = " + m.toString();
       });
-
+      
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
