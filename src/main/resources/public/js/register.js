00cbbbc7a71de6aeb20f6bede10b98be0deb5222
ruinan <run7@pitt.edu>
2016-02-17 23:18:18 -0500

    homework1

---
+++ src/main/resources/public/js/register.js
@@ -0,0 +1,11 @@
+function checkRegisterInformation(obj){
+	
+		if(obj.value === "" || obj.value === null){
+			obj.style.border = "2px solid #b30047";
+		}
+		else{
+			obj.style.border = "1px solid ";
+		}
+		
+
+}
